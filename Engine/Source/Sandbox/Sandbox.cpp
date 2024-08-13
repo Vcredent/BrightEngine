5aa2cbec8d01f3b48acea819bd8a7b695abaa30e
2B键盘 <2968640488@qq.com>
2024-08-13 22:09:14 +0800

    [Project]: Update all of files header comment format.

--- Engine/Source/Sandbox/Sandbox.cpp
+++ Engine/Source/Sandbox/Sandbox.cpp
@@ -1,29 +1,26 @@
-/* / ------------------------------------------------------------------------ \ */
-/* | Sandbox.cpp                                                              | */
-/* | ------------------------------------------------------------------------ | */
-/* |                        This file is part of:                             | */
-/* |                           TURBINE ENGINE                                 | */
-/* | ------------------------------------------------------------------------ | */
-/* |                                                                          | */
-/* | Copyright (C) 2022 Vcredent All rights reserved.                         | */
-/* |                                                                          | */
-/* | Licensed under the Apache License, Version 2.0 (the "License");          | */
-/* | you may not use this file except in compliance with the License.         | */
-/* |                                                                          | */
-/* | You may obtain a copy of the License at                                  | */
-/* |     http://www.apache.org/licenses/LICENSE-2.0                           | */
-/* |                                                                          | */
-/* | Unless required by applicable law or agreed to in writing, software      | */
-/* | distributed under the License is distributed on an "AS IS" BASIS,        | */
-/* | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied  | */
-/* | See the License for the specific language governing permissions and      | */
-/* | limitations under the License.                                           | */
-/* |                                                                          | */
-/* \ ------------------------------------------------------------------------ / */
-#include <Runtime/Drivers/RenderDevice.h>
-#include <Runtime/Window/Window.h>
+/* ------------------------------------------------------------------------ */
+/* Sandbox.h                                                                */
+/* ------------------------------------------------------------------------ */
+/*                        This file is part of:                             */
+/*                            BRIGHT ENGINE                                 */
+/* ------------------------------------------------------------------------ */
+/*                                                                          */
+/* Copyright (C) 2022 Vcredent All rights reserved.                         */
+/*                                                                          */
+/* Licensed under the Apache License, Version 2.0 (the "License");          */
+/* you may not use this file except in compliance with the License.         */
+/*                                                                          */
+/* You may obtain a copy of the License at                                  */
+/*     http://www.apache.org/licenses/LICENSE-2.0                           */
+/*                                                                          */
+/* Unless required by applicable law or agreed to in writing, software      */
+/* distributed under the License is distributed on an "AS IS" BASIS,        */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied  */
+/* See the License for the specific language governing permissions and      */
+/* limitations under the License.                                           */
+/*                                                                          */
+/* ------------------------------------------------------------------------ */
 #include <Runtime/Win32/RenderDeviceCotnextWin32.h>
-#include <Runtime/Renderer/ScreenRender.h>
 #include <ImGuiNav/ImGuiNav.h>
 
 int main()
